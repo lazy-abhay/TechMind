7449d153daf5f94b01839bedf9c2c8852c26a826
Abhay <abhaykrn02@gmail.com>
2025-04-09 22:29:23 +0530

    made utils and routes of the project

---
+++ Backend/routes/Course.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createCourse,
+  getAllCourses,
+  getCourseDetails,
+  getInstructorCourses,
+  editCourse,
+  getFullCourseDetails,
+  deleteCourse,
+  searchCourse,
+  markLectureAsComplete
+} = require("../controllers/Course");
+
+const {
+  showAllCategories,
+  createCategory,
+  categoryPageDetails,
+  addCourseToCategory
+} = require("../controllers/Category");
+
+const {
+  createSection,
+  updateSection,
+  deleteSection
+} = require("../controllers/Section");
+
+const {
+  createSubSection,
+  updateSubSection,
+  deleteSubSection
+} = require("../controllers/Subsection");
+
+const {
+  createRating,
+  getAverageRating,
+  getAllRating
+} = require("../controllers/RatingAndReviews");
+
+const { isDemo } = require("../middlewares/demo");
+const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth");
+
+router.post("/createCourse", auth, isInstructor, isDemo, createCourse);
+router.post("/addSection", auth, isInstructor, createSection);
+router.post("/updateSection", auth, isInstructor, updateSection);
+router.post("/deleteSection", auth, isInstructor, isDemo, deleteSection);
+router.post("/updateSubSection", auth, isInstructor, updateSubSection);
+router.post("/deleteSubSection", auth, isInstructor, deleteSubSection);
+router.post("/addSubSection", auth, isInstructor, createSubSection);
+router.get("/getAllCourses", getAllCourses);
+router.post("/getCourseDetails", getCourseDetails);
+router.post("/editCourse", auth, isInstructor, isDemo, editCourse);
+router.get("/getInstructorCourses", auth, isInstructor, getInstructorCourses);
+router.post("/getFullCourseDetails", auth, getFullCourseDetails);
+router.delete("/deleteCourse", auth, isDemo, deleteCourse);
+router.post("/searchCourse", searchCourse);
+router.post("/updateCourseProgress", auth, isStudent, markLectureAsComplete);
+
+router.post("/createCategory", auth, isAdmin, createCategory);
+router.get("/showAllCategories", showAllCategories);
+router.post("/getCategoryPageDetails", categoryPageDetails);
+router.post("/addCourseToCategory", auth, isInstructor, addCourseToCategory);
+
+router.post("/createRating", auth, isStudent, isDemo, createRating);
+router.get("/getAverageRating", getAverageRating);
+router.get("/getReviews", getAllRating);
+
+module.exports = router;
